12c45ebb3bd6497fbe6d3967143a91a15f6100b7
Roman Ilienko <ilienko.roman@gmail.com>
2012-09-12 21:13:56 +0300

    Core added

---
+++ iMessenger/Core.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iMessenger
+{
+    class Core
+    {
+        private MainWindow window;
+
+        public Core()
+        {
+            window = new MainWindow();
+            window.Show();
+        }
+    }
+}
